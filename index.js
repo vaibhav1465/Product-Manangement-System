d7d3ee12e76a17b7e92dbd2188ba176ef69da806
Vaibhav Kaushal <51777156+vaibhav1465@users.noreply.github.com>
2021-09-01 02:38:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-products=[];
+products=[
+    {
+        name:"Computer",
+        price:14662,
+        quantity:458
+    
+    },
+    {
+        name:"Mouse",
+        price:146684,
+        quantity:48
+    }];
 function add(){
     product={};
 
@@ -6,8 +17,32 @@ function add(){
     product.price=document.getElementById("price").value;
     product.quantity=document.getElementById("quantity").value;
     products.push(product);
-    console.log(products);
+    display();
     document.getElementById("name").value="";
     document.getElementById("price").value="";
     document.getElementById("quantity").value="";
+}
+function display(){
+    productString=`
+    <tr>
+    <th>#</th>
+    <th>Product Name</th>
+    <th>Price</th>
+    <th>Quantity</th>
+</tr>`;
+    for(let i=0;i<products.length;i++){
+        productString+=`
+        <tr>
+        <td>${i+1}</td>
+        <td>${products[i].name}</td>
+         <td>${products[i].price}</td>
+        <td>${products[i].quantity}</td>
+        <td>
+            <button class="btn">Update</button>
+            <button class="btn">Delete</button>
+        </td>
+    </tr>
+        `
+    }
+    document.getElementById("table").innerHTML=productString;
 }

2537089377b68e8518422c9d365beb6c6e695195
Vaibhav Kaushal <51777156+vaibhav1465@users.noreply.github.com>
2021-09-03 23:33:45 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -18,11 +18,14 @@ function add(){
     product.name=document.getElementById("name").value;
     product.price=document.getElementById("price").value;
     product.quantity=document.getElementById("quantity").value;
+    if(product.name!="" && product.price!=null && product.quantity!=null)
+    {
     products.push(product);
     display();
     document.getElementById("name").value="";
     document.getElementById("price").value="";
     document.getElementById("quantity").value="";
+   }
 }
 function display(){
     productString=`
@@ -54,7 +57,24 @@ deleteProduct=(index)=>{
 products.splice(index,1);
 display();
 }
+updateIndex=null;
 updateProduct=(index)=>{
     console.log("in update function")
     document.getElementById("updateform").style.display="flex";
+    let product={};
+    product=products[index];
+    updateIndex=index;
+    document.getElementById("upd-name").value=product.name;
+    document.getElementById("upd-price").value=product.price;
+    document.getElementById("upd-quantity").value=product.quantity;
+    
+}
+updateInfo=()=>{
+    let product={};
+    product.name=document.getElementById("upd-name").value;;
+    product.price=document.getElementById("upd-price").value;
+    product.quantity=document.getElementById("upd-quantity").value;
+    products[updateIndex]=product;
+    display();
+    document.getElementById("updateform").style.display="none";
 }
